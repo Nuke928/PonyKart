c53a31b6c7a5ba796fc2ddf1cccf56064e6c2760
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-05-05 00:50:49 +0200

    Improve the menu placeholder

--- Ponykart++/UI/MainMenuManager.cpp
+++ Ponykart++/UI/MainMenuManager.cpp
@@ -18,7 +18,13 @@ MainMenuManager::MainMenuManager()
 	mainWindow->addChild(dashbackground);
 
 	Window* checkers = WindowManager::getSingleton().createWindow("Generic/Image", "checkers");
-	checkers->setSize(USize(UDim(1, 0), UDim(1, 0)));
+	checkers->setSize(USize(UDim(0.5, 0), UDim(1, 0)));
 	checkers->setProperty("Image", "checkers/checkers");
 	mainWindow->addChild(checkers);
+
+	Window* playmenu = WindowManager::getSingleton().createWindow("Generic/Image", "playmenu");
+	playmenu->setPosition({ { 0.20f, 0 }, { 0.15f, 0 } });
+	playmenu->setSize(USize(UDim(0.60f, 0), UDim(0.85f, 0)));
+	playmenu->setProperty("Image", "playmenu/playmenu");
+	mainWindow->addChild(playmenu);
 }
