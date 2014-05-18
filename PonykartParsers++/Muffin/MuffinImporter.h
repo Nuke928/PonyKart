1a51948a32ba7d6dbf1ecf35f05fd43127fabe56
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-05-18 21:08:20 +0200

    Declare MuffinImporter,MuffinImporte::parseByName

---
+++ PonykartParsers++/Muffin/MuffinImporter.h
@@ -0,0 +1,16 @@
+#ifndef MUFFINIMPORTER_H_DEFINED
+#define MUFFINIMPORTER_H_DEFINED
+
+#include <string>
+#include "Muffin/MuffinDefinition.h"
+
+namespace PonykartParsers
+{
+	class MuffinImporter
+	{
+	public:
+		MuffinDefinition* parseByName(const std::string& nameOfMuffin, MuffinDefinition* worldDef = nullptr);
+	};
+}
+
+#endif // MUFFINIMPORTER_H_DEFINED
