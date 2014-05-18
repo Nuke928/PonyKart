fa236afaf4315ad614290d9e54d1a3fdae1cf380
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-05-18 22:06:45 +0200

    Define loadLevelHandlers to always throw
    Not implemented because too complicated at the moment. It relies on a
    mess of RTTI that we don't need at the moment. The priority is getting a
    bare level to load.

--- Ponykart++/Kernel/LKernelHandler.cpp
+++ Ponykart++/Kernel/LKernelHandler.cpp
@@ -1,3 +1,15 @@
 #include "pch.h"
 #include "Kernel/LKernelHandler.h"
+#include "Kernel/LKernelOgre.h"
+#include "Levels/Level.h"
 
+using namespace Ponykart;
+using namespace Ponykart::LKernel;
+using namespace Ponykart::Levels;
+using namespace std;
+
+void loadLevelHandlers(Levels::Level& newLevel)
+{
+	// TODO: Implement, requires implementing a RTTI mess
+	throw string("loadLevelHandlers(): Not implemented");
+}
